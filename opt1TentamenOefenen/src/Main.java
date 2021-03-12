56a70a4e0973aeb63e780d2d88cd8dae0e3fcb6d
Johan <20219059@student.hhs.nl>
2021-03-12 07:52:12 +0100

    eerste commit

---
+++ opt1TentamenOefenen/src/Main.java
@@ -0,0 +1,106 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+
+class Main {
+
+public static void main (String[] args){
+
+    Engine a1200 = new Engine (10, 80);
+
+    Train NS1 = new Train (a1200);
+
+    Coach coach3 = new Coach(40,250);
+    Coach coach4 = new Coach(20,200);
+    NS1.addCoach(coach3);
+    NS1.addCoach(coach4);
+
+
+    System.out.println(a1200.getWeight());
+    System.out.println(NS1.totalCapacity());
+
+}
+}
+
+
+class Engine{
+
+    private Integer weight;
+    private Integer power;
+
+    public Engine (Integer weight, Integer power)  {
+        this.weight = weight;
+        this.power = power;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public Integer getPower()  {
+        return power;
+    }
+}
+
+
+class Train{
+    private Engine isTowedBy;
+    private ArrayList<Coach> consistsOf ;
+
+    public Train(Engine engine) {
+        this.isTowedBy = engine;
+        this.consistsOf = new ArrayList<Coach>();
+
+    }
+
+
+    private Integer totalWeight() {
+
+        Integer totalWeight = 0;
+
+        for (int i=0; i < consistsOf.size(); i++) {
+            totalWeight += consistsOf.get(i).getWeight();
+        }
+        totalWeight += this.isTowedBy.getWeight();
+        return totalWeight;
+    }
+
+    public void addCoach (Coach coach){
+
+        Integer x = this.totalWeight();
+        x += coach.getWeight();
+        if ( x <= isTowedBy.getPower()) {
+            consistsOf.add(coach);
+
+    }
+
+    public Integer totalCapacity() {
+
+        Integer totalCapacity = 0;
+
+        for (int i=0; i < consistsOf.size(); i++) {
+            totalCapacity += consistsOf.get(i).getCapacity();
+
+        }
+        return totalCapacity;
+    }
+}
+
+class Coach{
+
+    private Integer weight;
+    private Integer capacity;
+
+    public Coach (Integer weight, Integer capacity){
+        this.weight = weight;
+        this.capacity = capacity;
+    }
+
+    public Integer getWeight() {
+        return this.weight;
+    }
+
+    public Integer getCapacity() {
+        return this.capacity;
+    }
+}
+
